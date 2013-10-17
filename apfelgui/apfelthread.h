24ddf42cd318cf35ae652c83f911f042b54cb9df
Stefano Carrazza <stefano.carrazza@mi.infn.it>
2013-10-17 11:55:45 +0000

    updating the gui

--- apfelgui/apfelthread.h
+++ apfelgui/apfelthread.h
@@ -7,17 +7,20 @@ namespace Ui {
   class MainWindow;
 }
 
+class QProgressDialog;
+
 class apfelthread : public QThread
 {
   Q_OBJECT
 
 public:
-  apfelthread(QObject *parent = 0, Ui::MainWindow *fui = 0);
+  apfelthread(QObject *parent = 0, Ui::MainWindow *fui = 0, QProgressDialog *fd = 0);
   ~apfelthread();
   void run();      //start calculation
 
 private:
   Ui::MainWindow *ui;
+  QProgressDialog *d;
 };
 
 #endif // APFELTHREAD_H
