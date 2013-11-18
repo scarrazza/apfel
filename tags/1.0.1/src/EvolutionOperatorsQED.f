686d4c82b0940b3f6d3675289e94bd6745516c23
bertone <bertone@2288560c-9ee3-4003-ac04-1924950ca0e7>
2013-11-18 14:08:01 +0000

    Small bug in the FFNS evolution when initial and final scales are equal fixed

--- tags/1.0.1/src/EvolutionOperatorsQED.f
+++ tags/1.0.1/src/EvolutionOperatorsQED.f
@@ -46,6 +46,8 @@
          nfi = Nf_FF
          nff = Nf_FF
          wnf = Nf_FF
+*
+         sgn = 1
 *     If initial and final energies are equal return immediately the intial conditions
          if(muF2.eq.muF20)then
             call EqualOperatorsQEDnf(Nf_FF,M0sg,M0nsp,M0nsm,
@@ -94,6 +96,7 @@
          if(muF2.eq.muF20)then
             call EqualOperatorsQEDnf(nfi,M0sg,M0nsp,M0nsm,
      1                                   MQEDsg,MQEDnsp,MQEDnsm)
+            sgn = 1
             return
          elseif(muF2.gt.muF20)then
             sgn = 1
