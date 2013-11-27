58594075f52e9ce29744505b8547e3795fe977aa
carrazza <carrazza@2288560c-9ee3-4003-ac04-1924950ca0e7>
2013-11-27 14:27:56 +0000

    updating main

--- trunk/apfelgui2/src/main.cxx
+++ trunk/apfelgui2/src/main.cxx
@@ -1,8 +1,13 @@
 #include "apfelmainwindow.h"
 #include <QApplication>
+#include <QLocale>
 
 int main(int argc, char *argv[])
 {
+  QLocale loc = QLocale::system(); // current locale
+  loc.setNumberOptions(QLocale::c().numberOptions()); // borrow number options from the "C" locale
+  QLocale::setDefault(loc); // set as default
+ 
   QApplication a(argc, argv);
   APFELMainWindow w;
   w.show();
