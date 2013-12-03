a7100fc8450a4de6a2d8b1b6d3686ca3d18739c1
carrazza <carrazza@2288560c-9ee3-4003-ac04-1924950ca0e7>
2013-12-03 10:54:00 +0000

    updating gui

--- trunk/apfelgui2/src/plotcomparison.cxx
+++ trunk/apfelgui2/src/plotcomparison.cxx
@@ -206,8 +206,8 @@ void comparisonthread::run()
   double *x = new double[N];
   for (int i = 0; i < N; i++)
     {
-      if (fp->ui->logx->isChecked()) x[i] = exp(log(xmin)+i*(log(xmax)-log(xmin))/N);
-      else x[i] = xmin+i*(xmax-xmin)/N;
+      if (fp->ui->logx->isChecked()) x[i] = exp(log(xmin)+i*(log(xmax)-log(xmin))/(N-1));
+      else x[i] = xmin+i*(xmax-xmin)/(N-1);
     }
 
   vector<int> indRef;
