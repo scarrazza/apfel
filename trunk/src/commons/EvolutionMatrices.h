acff52097521c4e16ee4d0898e03b55181700cbe
bertone <bertone@2288560c-9ee3-4003-ac04-1924950ca0e7>
2014-10-31 14:45:26 +0000

    size of some arrays reduced

--- trunk/src/commons/EvolutionMatrices.h
+++ trunk/src/commons/EvolutionMatrices.h
@@ -4,30 +4,30 @@
 *
       integer nfi,nff,sgn
 
-      double precision MQCDsg(3:6,2,2,0:nint_max,0:nint_max)
-      double precision MQCDnsp(3:6,0:nint_max,0:nint_max)
-      double precision MQCDnsm(3:6,0:nint_max,0:nint_max)
-      double precision MQCDnsv(3:6,0:nint_max,0:nint_max)
+      real MQCDsg(3:6,2,2,0:nint_max,0:nint_max)
+      real MQCDnsp(3:6,0:nint_max,0:nint_max)
+      real MQCDnsm(3:6,0:nint_max,0:nint_max)
+      real MQCDnsv(3:6,0:nint_max,0:nint_max)
 
-      double precision dMQCDsg(2,2,0:nint_max,0:nint_max)
-      double precision dMQCDnsp(0:nint_max,0:nint_max)
-      double precision dMQCDnsm(0:nint_max,0:nint_max)
-      double precision dMQCDnsv(0:nint_max,0:nint_max)
+      real dMQCDsg(2,2,0:nint_max,0:nint_max)
+      real dMQCDnsp(0:nint_max,0:nint_max)
+      real dMQCDnsm(0:nint_max,0:nint_max)
+      real dMQCDnsv(0:nint_max,0:nint_max)
 
-      double precision MQEDsg(3:6,3,3,0:nint_max,0:nint_max)
-      double precision MQEDnsp(3:6,0:nint_max,0:nint_max)
-      double precision MQEDnsm(3:6,0:nint_max,0:nint_max)
+      real MQEDsg(3:6,3,3,0:nint_max,0:nint_max)
+      real MQEDnsp(3:6,0:nint_max,0:nint_max)
+      real MQEDnsm(3:6,0:nint_max,0:nint_max)
 
-      double precision dMQEDsg(3,3,0:nint_max,0:nint_max)
-      double precision dMQEDnsp(0:nint_max,0:nint_max)
-      double precision dMQEDnsm(0:nint_max,0:nint_max)
+      real dMQEDsg(3,3,0:nint_max,0:nint_max)
+      real dMQEDnsp(0:nint_max,0:nint_max)
+      real dMQEDnsm(0:nint_max,0:nint_max)
 
-      double precision MUnisg1(3:6,4,4,0:nint_max,0:nint_max)
-      double precision MUnisg2(3:6,2,2,0:nint_max,0:nint_max)
-      double precision MUninspu(3:6,0:nint_max,0:nint_max)
-      double precision MUninspd(3:6,0:nint_max,0:nint_max)
-      double precision MUninsmu(3:6,0:nint_max,0:nint_max)
-      double precision MUninsmd(3:6,0:nint_max,0:nint_max)
+      real MUnisg1(3:6,4,4,0:nint_max,0:nint_max)
+      real MUnisg2(3:6,2,2,0:nint_max,0:nint_max)
+      real MUninspu(3:6,0:nint_max,0:nint_max)
+      real MUninspd(3:6,0:nint_max,0:nint_max)
+      real MUninsmu(3:6,0:nint_max,0:nint_max)
+      real MUninsmd(3:6,0:nint_max,0:nint_max)
 *
       common / ActiveFlavAPFEL / nfi,nff,sgn
       common / EvolMatQCDAPFEL / MQCDsg,MQCDnsp,MQCDnsm,MQCDnsv,
