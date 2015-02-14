39927a237336e02372f65dda2353e1d8ed44b79e
Valerio Bertone <valerio.bertone@cern.ch>
2015-02-14 15:30:23 +0000

    working on the listing function

---
+++ examples/ListFunctions.f
@@ -0,0 +1,12 @@
+************************************************************************
+*
+*     ListFunctions.f:
+*
+*     program that lists the APFEL functions.
+*
+************************************************************************
+      program ListFuncs
+*
+      call ListFunctions
+*
+      end
---
+++ examples/ListFunctionsCxx.cc
@@ -0,0 +1,19 @@
+/*
+ * ListFunctions.f:
+ *
+ * Program that lists the APFEL functions.
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <fstream>
+#include "APFEL/APFEL.h"
+using namespace std;
+
+int main()
+{
+  APFEL::ListFunctions();
+
+  return 0;
+}
