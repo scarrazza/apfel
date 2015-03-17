d76dc60e6ff34383e2bd0942dc2dacd25086eb4e
Valerio Bertone <valerio.bertone@cern.ch>
2015-03-17 19:14:19 +0100

    new (x,Q2)-grid for the LHAPDF output grids implemented

--- src/Evolution/LHAPDFgrid.f
+++ src/Evolution/LHAPDFgrid.f
@@ -17,6 +17,8 @@
       include "../commons/kren.h"
       include "../commons/Th.h"
       include "../commons/LHAgrid.h"
+      include "../commons/Evs.h"
+      include "../commons/MaxFlavourPDFs.h"
 **
 *     Input Variables
 *
@@ -28,20 +30,25 @@
 *     Intenal Variables
 *
       integer ln
-      integer i,ix,iq2,ipdf,krep
+      integer i,ix,iq2,iq2c,ipdf,krep,iq2in,iq2fi
+      integer nfin
+      integer isg,nQ(3:7)
       double precision qref,mth(4:6)
-      double precision lref,lambda3,lambda4,lambda5,lambdaNF
+      double precision lambda3,lambda4,lambda5,lambdaNF
       double precision xbLHA(nxLHA),q2LHA(nq2LHA)
+      double precision lnQmin,lnQmax
+      double precision eps,offset
       double precision xpdfLHA(-6:6,nxLHA,nq2LHA)
       double precision xgammaLHA(nxLHA,nq2LHA)
-      double precision xPDF,xgamma
+      double precision xPDFj,xgammaj
       double precision AlphaQCD
       character*30 str
-      parameter(lref=0.239d0)
+      parameter(eps=1d-10)
+      parameter(offset=1d-2)
 *
 *     Specify initialization for the LHgrid
 *
-      call SetQLimits(dsqrt(q2minLHA),dsqrt(q2maxLHA))
+      call SetQLimits(dsqrt(q2minLHA)-offset,dsqrt(q2maxLHA)+offset)
       call SetNumberOfGrids(3)
       call SetGridParameters(1,100,3,xminLHA)
       call SetGridParameters(2,50,5,xmLHA)
@@ -51,12 +58,99 @@
 *
 *     Compute the values of lambdaQCD
 *
-c      lambda5 = lref
-c      call lambda(4,lref,lambda4)
-c      call lambda(3,lref,lambda3)
       lambda5 = lambdaNF(5,alpha_ref_qcd,q2_ref_qcd)
       lambda4 = lambdaNF(4,alpha_ref_qcd,q2_ref_qcd)
       lambda3 = lambdaNF(3,alpha_ref_qcd,q2_ref_qcd)
+*
+*     Compute x-space grid
+*
+      do ix=1,nxLHA
+         if(ix.le.nxmLHA)then
+            xbLHA(ix) = xminLHA * ( xmLHA / xminLHA )
+     1           **( 2d0 * dble( ix-1 ) / dble( nxLHA - 1 ) )
+         else
+            xbLHA(ix) = xmLHA + ( xmaxLHA - xmLHA ) 
+     1           * ( dble( ix - nxmLHA - 1 ) 
+     2           / dble( nxLHA - nxmLHA - 1 ) )
+         endif
+      enddo
+*
+*     Compute Q2 grid.
+*     Use a distribution of the Q2 nodes uniform in ln(ln(Q2/Lambda2))
+*     and has nodes on the heavy quark thresholds.
+*
+      lnQmin = dlog( q2minLHA / Lambda2 )
+      lnQmax = dlog( q2maxLHA / Lambda2 )
+*
+      if(Evs.eq."VF")then
+         if(q2minLHA.gt.m2th(6))then
+            nfin = 6
+         elseif(q2minLHA.gt.m2th(5))then
+            nfin = 5
+         elseif(q2minLHA.gt.m2th(4))then
+            nfin = 4
+         else
+            nfin = 3
+         endif
+         if(nfin.gt.nfMaxPDFs) nfin = nfMaxPDFs
+*
+*     Number of points per subgrid
+*
+         do isg=3,7
+            nQ(isg) = 0
+         enddo
+*
+         isg = nfin
+         do iq2=1,nq2LHA
+            q2LHA(iq2) = Lambda2 * dexp( lnQmin 
+     1                 * dexp( dble( iq2 - 1 ) / dble( nq2LHA - 1 )
+     2                 * dlog( lnQmax / lnQmin ) ) )
+            if(q2LHA(iq2).lt.m2th(isg+1))then
+               nQ(isg) = nQ(isg) + 1
+            else
+               isg = isg + 1
+               if(isg.gt.nfMaxPDFs) isg = nfMaxPDFs
+               nQ(isg) = nQ(isg) + 1
+            endif
+         enddo
+*
+         lnQmin = dlog( ( q2minLHA + eps ) / Lambda2 )
+         if(nfin.eq.nfMaxPDFs)then
+            lnQmax = dlog( q2maxLHA / Lambda2 )
+         else
+            lnQmax = dlog( ( m2th(nfin+1) - eps ) / Lambda2 )
+         endif
+*
+         iq2c = 0
+         do isg=nfin,nfMaxPDFs
+            do iq2=1,nQ(isg)
+               iq2c = iq2c + 1
+               q2LHA(iq2c) = Lambda2 * dexp( lnQmin 
+     1                     * dexp( dble( iq2 - 1 ) / dble( nQ(isg) - 1 )
+     2                     * dlog( lnQmax / lnQmin ) ) )
+            enddo
+            lnQmin = dlog( ( m2th(isg+1) + eps ) / Lambda2 )
+            if(isg.eq.nfMaxPDFs-1)then
+               lnQmax = dlog( q2maxLHA / Lambda2 )
+            else
+               lnQmax = dlog( ( m2th(isg+2) - eps ) / Lambda2 )
+            endif
+         enddo
+*
+         if(iq2c.ne.nq2LHA)then
+            write(6,*) "In LHAPDFgrid.f:"
+            write(6,*) "Mismatch in the Number of Q2 nodes"
+            write(6,*) "- Expected = ",nq2LHA
+            write(6,*) "- Found = ",iq2c
+            call exit(-10)
+         endif
+      else
+         do iq2=1,nq2LHA
+            q2LHA(iq2) = Lambda2 * dexp( lnQmin 
+     1                 * dexp( dble( iq2 - 1 ) / dble( nq2LHA - 1 )
+     2                 * dlog( lnQmax / lnQmin ) ) )
+         enddo
+      endif
 *
 *     LHAPDF5 output
 *
@@ -132,22 +226,12 @@ c      call lambda(3,lref,lambda3)
 *     
          write(13,*) nxLHA
          do ix=1,nxLHA
-            if(ix.le.nxmLHA)then
-               xbLHA(ix) = xminLHA * ( xmLHA / xminLHA )
-     1              **( 2d0 * dble( ix - 1 ) / dble( nxLHA - 1 ) )
-            else
-               xbLHA(ix) = xmLHA + ( xmaxLHA - xmLHA )
-     1              * ( dble( ix - nxmLHA - 1 )
-     2              / dble( nxLHA - nxmLHA - 1 ) )
-            endif
             write(13,*) xbLHA(ix)
          enddo
 *     
          write(13,*) nq2LHA
          write(13,*) q2minLHA
          do iq2=1,nq2LHA
-            q2LHA(iq2) = q2minLHA * ( q2maxLHA / q2minLHA )
-     1           **( dble( iq2 - 1 ) / dble( nq2LHA - 1 ) )
             write(13,*) q2LHA(iq2)
          enddo
 *     
@@ -161,9 +245,9 @@ c      call lambda(3,lref,lambda3)
                call EvolveAPFEL(Qin,dsqrt(q2LHA(iq2)))
                do ix=1,nxLHA
                   do ipdf=-6,6
-                     xpdfLHA(ipdf,ix,iq2) = xPDF(ipdf,xbLHA(ix))
+                     xpdfLHA(ipdf,ix,iq2) = xPDFj(ipdf,xbLHA(ix))
                   enddo
-                  xgammaLHA(ix,iq2) = xgamma(xbLHA(ix))
+                  xgammaLHA(ix,iq2) = xgammaj(xbLHA(ix))
                enddo
             enddo
 *     
@@ -177,7 +261,7 @@ c      call lambda(3,lref,lambda3)
                do ix=1,nxLHA
                   do iq2=1,nq2LHA
                      write(13,45) (xpdfLHA(ipdf,ix,iq2),ipdf=-6,6),
-     1                    xgammaLHA(ix,iq2)
+     1                             Xgammalha(ix,iq2)
                   enddo
                enddo
             endif
@@ -233,37 +317,19 @@ c      call lambda(3,lref,lambda3)
          write(13,*) "AlphaS_MZ:",alpha_ref_qcd
          write(13,*) "AlphaS_OrderQCD:",ipt
          write(13,*) "AlphaS_Type: ipol"
-         
-         do ix=1,nxLHA
-            if(ix.le.nxmLHA)then
-               xbLHA(ix) = xminLHA * ( xmLHA / xminLHA )
-     1              **( 2d0 * dble( ix-1 ) / dble( nxLHA - 1 ) )
-            else
-               xbLHA(ix) = xmLHA + ( xmaxLHA - xmLHA )
-     1              * ( dble( ix - nxmLHA - 1 )
-     2              / dble( nxLHA - nxmLHA - 1 ) )
-            endif
-         enddo
-
-         do iq2=1,nq2LHA
-            q2LHA(iq2) = q2minLHA * ( q2maxLHA / q2minLHA )
-     1           **( dble( iq2 - 1 ) / dble( nq2LHA - 1 ) )
-         enddo
-
          write(13,*)"AlphaS_Qs: [",(dsqrt(q2LHA(iq2)),",",iq2=1,nq2LHA),
-     1        "]" 
-         write(13,*) "AlphaS_Vals: [", 
-     1        (AlphaQCD(dsqrt(q2LHA(iq2))),",",iq2=1,nq2LHA), "]"
+     1              "]" 
+         write(13,*) "AlphaS_Vals: [",(AlphaQCD(dsqrt(q2LHA(iq2))),
+     1               ",",iq2=1,nq2LHA), "]"
          write(13,*) "AlphaS_Lambda4:", lambda4
          write(13,*) "AlphaS_Lambda5:", lambda5
-         
          close(13)
 *
 *     Now Loop over all replicas and print to file
 *
          do krep=0,Nrep
             write(6,*) "Evaluating replica",krep," ..."            
-
+*
             if (krep.lt.10) then
                write(str,'(i1)' ) krep
                open(unit=13,status="unknown",file=fname(1:ln)//"/"
@@ -288,41 +354,49 @@ c      call lambda(3,lref,lambda3)
             endif
             write(13,*) "Format: lhagrid1"
             write(13,*) "---"
-            write(13,*) (xbLHA(ix), ix=1,nxLHA)
-            write(13,*) (dsqrt(q2LHA(iq2)), iq2=1,nq2LHA)
-*
-            if(Th.eq."QCD") then
-               write(13,"(a)") "-6 -5 -4 -3 -1 -2 21 2 1 3 4 5 6"
-            else
-               write(13,"(a)") "-6 -5 -4 -3 -1 -2 21 2 1 3 4 5 6 22"
-            endif
 *
             call SetReplica(krep)
-            do iq2=1,nq2LHA
-               call EvolveAPFEL(Qin,dsqrt(q2LHA(iq2)))
-               do ix=1,nxLHA
-                  do ipdf=-6,6
-                     xpdfLHA(ipdf,ix,iq2) = xPDF(ipdf,xbLHA(ix))
+            iq2in = 1
+            iq2fi = nQ(nfin)
+            do isg=nfin,nfMaxPDFs
+               write(13,*) (xbLHA(ix),ix=1,nxLHA)
+c               write(13,*) (dsqrt(q2LHA(iq2)), iq2=1,nq2LHA)
+               write(13,*) (dsqrt(q2LHA(iq2)), iq2=iq2in,iq2fi)
+*
+               if(Th.eq."QCD") then
+                  write(13,"(a)") "-6 -5 -4 -3 -1 -2 21 2 1 3 4 5 6"
+               else
+                  write(13,"(a)") "-6 -5 -4 -3 -1 -2 21 2 1 3 4 5 6 22"
+               endif
+*
+               do iq2=iq2in,iq2fi
+                  call EvolveAPFEL(Qin,dsqrt(q2LHA(iq2)))
+                  do ix=1,nxLHA
+                     do ipdf=-6,6
+                        xpdfLHA(ipdf,ix,iq2) = xPDFj(ipdf,xbLHA(ix))
+                     enddo
+                     xgammaLHA(ix,iq2) = xgammaj(xbLHA(ix))
                   enddo
-                  xgammaLHA(ix,iq2) = xgamma(xbLHA(ix))
                enddo
-            enddo
 *     
-            if(Th.eq."QCD")then
-               do ix=1,nxLHA
-                  do iq2=1,nq2LHA
-                     write(13,44) (xpdfLHA(ipdf,ix,iq2),ipdf=-6,6)
+               if(Th.eq."QCD")then
+                  do ix=1,nxLHA
+                     do iq2=iq2in,iq2fi
+                        write(13,44) (xpdfLHA(ipdf,ix,iq2),ipdf=-6,6)
+                     enddo
                   enddo
-               enddo
-            else
-               do ix=1,nxLHA
-                  do iq2=1,nq2LHA
-                     write(13,45) (xpdfLHA(ipdf,ix,iq2),ipdf=-6,6),
-     1                    xgammaLHA(ix,iq2)
+               else
+                  do ix=1,nxLHA
+                     do iq2=iq2in,iq2fi
+                        write(13,45) (xpdfLHA(ipdf,ix,iq2),ipdf=-6,6),
+     1                       xgammaLHA(ix,iq2)
+                     enddo
                   enddo
-               enddo
-            endif
-            write(13,*) "---"
+               endif
+               write(13,*) "---"
+               iq2in = iq2in + nQ(isg)
+               iq2fi = iq2fi + nQ(isg+1)
+            enddo
             close(13)
          enddo
 *     
--- src/commons/LHAgrid.h
+++ src/commons/LHAgrid.h
@@ -5,12 +5,14 @@
       integer nxLHA,nxmLHA,nq2LHA
       double precision q2minLHA,q2maxLHA
       double precision xminLHA,xmLHA,xmaxLHA
+      double precision Lambda2
 *
       parameter(nxLHA    = 100)
       parameter(nxmLHA   = 50)
       parameter(nq2LHA   = 50)
-      parameter(q2minLHA = 2d0)
-      parameter(q2maxLHA = 1d8)
-      parameter(xminLHA  = 1d-7)
+      parameter(q2minLHA = 1d0)
+      parameter(q2maxLHA = 1d10)
+      parameter(xminLHA  = 1d-9)
       parameter(xmLHA    = 1d-1)
       parameter(xmaxLHA  = 1d0)
+      parameter(Lambda2  = 0.0625d0)
