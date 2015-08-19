30c7f947573a660c6ecee6c0150fc34af12c8c8f
Valerio Bertone <valerio.bertone@cern.ch>
2015-08-19 17:52:05 +0200

    Reorganization of the documentation

---
+++ doc/sources/ProduceDocs.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+for ((index=0; index<2; index++))
+do
+    pdflatex CC_observables_def.tex
+    pdflatex Complex_DGLAP.tex
+    pdflatex DIS.tex
+    pdflatex Evolution_code.tex
+    pdflatex Lagrange_derivative.tex
+    pdflatex QCD_QED_common_basis.tex
+    pdflatex TruncatedSolution.tex
+    pdflatex manual.tex
+    pdflatex running_mass.tex
+done
+
+mv *.pdf ../pdfs/.
+rm *.aux *.log *.out *.toc *.idx
