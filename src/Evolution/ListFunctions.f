806ae4f9b395022598157f51ad0feb24cbc68b84
Valerio Bertone <valerio.bertone@cern.ch>
2017-09-21 13:52:00 +0200

    updating list-func function

--- src/Evolution/ListFunctions.f
+++ src/Evolution/ListFunctions.f
@@ -416,10 +416,11 @@
      1           achar(27)//"[0m"
       write(6,*) "    enables or disables the damping factor when the"
       write(6,*) "    FONLL structure functions are computed."
-      write(6,*) achar(27)//"[34m- SetDampingPowerFONLL(int):"//
-     1           achar(27)//"[0m"
+      write(6,*) achar(27)//"[34m- SetDampingPowerFONLL(int dpc,",
+     1     " int dpb, int dpt,):"//achar(27)//"[0m"
       write(6,*) "    set the power with which damping factor"
-      write(6,*) "    suppresses the subleading terms."
+      write(6,*) "    suppresses the subleading terms for charm,"
+      write(6,*) "    bottom, and top, separately."
       write(6,*) achar(27)//"[34m- SelectCharge(string selch):"//
      1           achar(27)//"[0m"
       write(6,*) "    selects one particular charge in the NC structure"
