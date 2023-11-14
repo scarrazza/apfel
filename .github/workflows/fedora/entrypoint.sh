d2419f73c234dcc39967e08cab27b2e99e7afb66
Andrii Verbytskyi <andriish@pcatlas18.mpp.mpg.de>
2023-11-14 11:50:10 +0100

    Fix LHAPDF

--- .github/workflows/fedora/entrypoint.sh
+++ .github/workflows/fedora/entrypoint.sh
@@ -8,4 +8,4 @@ cmake -S . -B BUILD -DCMAKE_INSTALL_PREFIX=$(pwd)/../INSTALL || out=1
 cmake --build BUILD -j 2 || out=1
 cmake --install BUILD || out=1
 ctest --test-dir BUILD -j 2 || out=1
-echo ::set-output fineapfel=out::$out
+echo ::set-output name=out::$out
