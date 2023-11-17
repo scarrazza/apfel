663f2a231a97a87a9111e61627f71afef1478881
Andrii Verbytskyi <andriish@pcatlas18.mpp.mpg.de>
2023-11-17 14:07:59 +0100

    Cleanup

--- .github/workflows/osx/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh -l
-set -x
-brew install wget coreutils gcc swig
-brew tap davidchall/hep
-brew install lhapdf
-cmake -S . -B BUILD -DCMAKE_INSTALL_PREFIX=$(pwd)/../INSTALL -DCMAKE_Fortran_COMPILER=gfortran-13   || exit 1
-cmake --build BUILD -j 2 || exit 1
-cmake --install BUILD || exit 1
-ctest --test-dir BUILD -j 2 || exit 1
